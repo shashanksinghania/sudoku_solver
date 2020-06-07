e09c72153db9f499e1ebb2a899cbf7a01ee7af2e
shashank <shashanks@gatech.edu>
2020-06-08 01:13:58 +0530

    Functions to check validity and empty spots added

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,7 +1,7 @@
 board = [
-    [9, 1, 4, 5, 7, 3, 8, 2, 6],
-    [5, 3, 2, 4, 8, 6, 1, 7, 9],
-    [9, 1, 3, 4, 2, 5, 8, 6, 7],
+    [9, 1, 5, 5, 7, 3, 8, 2, 6],
+    [5, 1, 2, 2, 8, 6, 1, 7, 9],
+    [4, 1, 3, 6, 2, 5, 8, 6, 7],
     [7, 1, 8, 2, 9, 3, 4, 5, 6],
     [9, 7, 1, 3, 2, 6, 5, 8, 4],
     [5, 3, 1, 7, 4, 2, 9, 8, 6],
@@ -11,17 +11,49 @@ board = [
 ]
 
 
+# this function prints the board
 def print_board(bd):
     for i in range(len(bd)):
         if i % 3 == 0 and not i == 0:
-            print("-----------------------------")
+            print('-----------------------------')
         for j in range(len(bd[0])):
             if j % 3 == 0 and not j == 0:
                 print('| ', end="")
             if j == 8:
-                print(bd[i][j]," ")
+                print(bd[i][j], " ")
             else:
-                print(bd[i][j]," ", end="")
+                print(bd[i][j], " ", end="")
 
 
-print_board(board)
+# this function finds the next empty spot and returns the index as a tuple
+def get_empty_spots(bd):
+    for i in range(len(bd)):
+        for j in range(len(bd[0])):
+            if bd[i][j] == 0:
+                return (i, j)
+    return None
+
+
+# this function checks if a number is valid at that a given position
+def is_valid(bd, number: int, position: tuple):
+    # Checking if the row is valid
+    for i in range(len(bd)):
+        if position[1] != i and bd[position[0]][i] == number:
+            return False
+
+    # Checking if column is valid
+    for i in range(len(bd)):
+        if position[0] != i and bd[i][position[1]] == number:
+            return False
+
+    # Check the square
+    for i in range(-2, 3):
+        if position[0] // 3 == (position[0] + i) // 3:
+            for j in range(0, 3):
+                if i == 0 and j == 0:
+                    continue
+                if position[1] // 3 == (position[1] + j) // 3 and bd[position[0] + i][position[1] + j] == number:
+                    return False
+                if position[1] // 3 == (position[1] - j) // 3 and bd[position[0] + i][position[1] - j] == number:
+                    return False
+    return True
